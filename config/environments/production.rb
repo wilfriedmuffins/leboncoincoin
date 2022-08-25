9ca8d636d7b275ee60610dbe46ba7178799345fc
wilfriedmuffins <wilfried.moffen@live.fr>
2022-08-25 09:13:05 +0200

    :lock: config sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,23 +94,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://leboncoincoincoin.herokuapp.com' }
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  #   :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-  #   :domain => 'leboncoincoincoin.herokuapp.com',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: "herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"]
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'leboncoincoincoin.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
+
 end
