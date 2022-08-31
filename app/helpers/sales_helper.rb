cd30db519e9f39aa445a70d0fa4e908292ff932e
wilfriedmuffins <wilfried.moffen@live.fr>
2022-08-31 14:41:49 +0200

    :bug: create ad

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,12 +1,3 @@
 module SalesHelper
-    def state_of_sale(sale)
-        case sale
-        when true  
-            "sold ✅ "
-        when false 
-            "on sale 🔄"
-        else
-            nil
-        end
-    end
+   
 end
